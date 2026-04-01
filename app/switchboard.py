cc1d8cee941c15fde44436092f21343e8fa2a255
Danil.S <silex2dev@gmail.com>
2026-04-01 21:28:11 +0700

    Done homework

--- app/switchboard.py
+++ app/switchboard.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from app.users import User
+from app.users import User, ForeignUser, LocalUser
 
 
 LOCAL_PHONE_PREFIX = "+7"
@@ -21,6 +21,7 @@ class ActiveCall:
 class Switchboard:
     def __init__(self) -> None:
         self._active_calls: list[ActiveCall] = []
+        self._cross_border_count = 0
 
     def register_call(self, raw_call: str) -> ActiveCall:
         '''
@@ -29,10 +30,34 @@ class Switchboard:
 
         Например: "1001,Иван Петров,+71234567890,1085,Адам Яковлев,+71255556666"
         '''
-        pass  # Удалите `pass` и пишите ваш код
+        strs = raw_call.split(",")
+        
+        if len(strs) != 6:
+            raise AttributeError(f"Input string must contain 6 values separated by ',', actual: {raw_call}")
+        
+        c_id, c_name, c_phone, r_id, r_name, r_phone = strs;
+        if not (c_id.isnumeric() and int(c_id) >= 0) or not (r_id.isnumeric() and int(r_id) >= 0):
+            raise AttributeError(f"Input string must contain 2 positive int id, actual: {raw_call}")
+
+        if len(c_name) == 0 or len(r_name) == 0:
+            raise AttributeError(f"Input string must contain non-empty names, actual: {raw_call}")
+        if (len(c_phone) == 0 or not c_phone.startswith("+")) \
+            and (len(r_phone) or not c_phone.startswith("+")):
+                raise AttributeError(f"Input string must contain valid phone number, actual: {raw_call}")
+
+            
+        caller = LocalUser(int(c_id), c_name, c_phone) if c_phone.startswith(LOCAL_PHONE_PREFIX) else ForeignUser(int(c_id), c_name, c_phone)
+        receiver = LocalUser(int(r_id), r_name, r_phone) if r_phone.startswith(LOCAL_PHONE_PREFIX) else ForeignUser(int(r_id), r_name, r_phone)
+        
+        newCall = ActiveCall(caller, receiver)
+        if newCall.is_cross_border:
+            self._cross_border_count += 1
+
+        self._active_calls.append(newCall)
+        return newCall
 
     def get_active_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return len(self._active_calls)
 
     def get_cross_border_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return self._cross_border_count
--- tests/test_switchboard.py
+++ tests/test_switchboard.py
@@ -45,3 +45,72 @@ def test_register_call_counts_calls_between_local_and_foreign_users() -> None:
 
     assert switchboard.get_active_calls_count() == 3
     assert switchboard.get_cross_border_calls_count() == 1
+
+def test_register_call_not_counts_cross_border() -> None:
+    switchboard = Switchboard()
+
+    switchboard.register_call(
+        "1,Ivan Ivanov,+79990000000,2,Petr Petrov,+78880000000"
+    )
+
+    assert switchboard.get_cross_border_calls_count() == 0
+
+def test_register_call_raise_error_with_incorrect_arguments() -> None:
+    switchboard = Switchboard()
+
+    with pytest.raises(AttributeError, 
+                       match="Input string must contain 6 values separated by ',', actual: *"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,+79990000000,2,Petr Petrov+78880000000"
+        )
+
+def test_register_call_has_no_active_at_init() -> None:
+    switchboard = Switchboard()
+
+    assert switchboard.get_active_calls_count() == 0
+    assert switchboard.get_cross_border_calls_count() == 0
+
+def test_register_call_raise_error_with_incorrect_id() -> None:
+    switchboard = Switchboard()
+
+    with pytest.raises(AttributeError, 
+                       match="Input string must contain 2 positive int id, actual: *"):
+        switchboard.register_call(
+            "1asd,Ivan Ivanov,+79990000000,2,Petr Petrov,+78880000000"
+        )
+
+def test_register_call_raise_error_with_negative_id() -> None:
+    switchboard = Switchboard()
+
+    with pytest.raises(AttributeError, 
+                       match="Input string must contain 2 positive int id, actual: *"):
+        switchboard.register_call(
+            "-1,Ivan Ivanov,+79990000000,2,Petr Petrov,+78880000000"
+        )
+
+def test_register_call_raise_error_with_empty_name() -> None:
+    switchboard = Switchboard()
+
+    with pytest.raises(AttributeError, 
+                       match="Input string must contain non-empty names, actual: *"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,+79990000000,2,,+78880000000"
+        )
+
+def test_register_call_raise_error_with_no_country_code() -> None:
+    switchboard = Switchboard()
+
+    with pytest.raises(AttributeError, 
+                       match="Input string must contain valid phone number, actual: *"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,999990000000,2,Petr Petrov,+78880000000"
+        )
+    
+def test_register_call_raise_error_with_empty_phone() -> None:
+    switchboard = Switchboard()
+
+    with pytest.raises(AttributeError, 
+                       match="Input string must contain valid phone number, actual: *"):
+        switchboard.register_call(
+            "1,Ivan Ivanov,,2,Petr Petrov,+78880000000"
+        )
